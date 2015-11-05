0ae311796553e371624859659d41ed356ecb85b3
Ley Al-Khalaki <7236085@gmail.com>
2015-11-05 16:43:00 +0400

    initial commit

---
+++ app/Http/routes.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+
+//$app['admin.auth'] = $app['auth'];
+
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/home', function () {
+    return view('welcome');
+});
+
+// Authentication routes...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
+
+/*
+ * Тестирование ЛК
+ */
+
+Route::get('/lk', function () {
+    echo "закрытая страница";
+})->middleware(['auth']);
