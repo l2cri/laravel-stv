1d35209950bcea6c1f46423d2ac6d3e9c37498dc
Ley Al-Khalaki <7236085@gmail.com>
2016-01-13 16:45:44 +0400

    beauty errors

--- resources/views/errors.blade.php
+++ resources/views/errors.blade.php
@@ -1,7 +1,11 @@
 @if ($errors->has())
-    <div class="alert alert-danger">
-        @foreach ($errors->all() as $error)
-            {{ $error }}<br>
-        @endforeach
+    <div class="message-box message-danger">
+        <div class="message-icon"><i class="fa fa-times"></i></div>
+        <div class="message-text"><b>Ошибка!</b>
+            @foreach ($errors->all() as $error)
+                {{ $error }}<br>
+            @endforeach
+        </div>
+        <div class="message-close"><i class="fa fa-times"></i></div>
     </div>
 @endif
