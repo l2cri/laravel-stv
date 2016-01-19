7b53ede988506287cfc43c2aa1a38930cb40dde7
Ley Al-Khalaki <7236085@gmail.com>
2016-01-20 01:41:45 +0400

    product repo start

---
+++ app/Repo/Product/EloquentProduct.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ley
+ * Date: 19.01.16
+ * Time: 20:51
+ */
+
+namespace App\Repo\Product;
+
+
+class EloquentProduct
+{
+
+}
---
+++ app/Repo/Product/ProductInterface.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ley
+ * Date: 19.01.16
+ * Time: 20:51
+ */
+
+namespace App\Repo\Product;
+
+
+interface ProductInterface
+{
+    public function byId($id);
+    public function create(array $data);
+    public function update(array $data);
+    public function delete($id);
+
+    public function bySupplier($supplierId);
+    public function bySection($sectionId, $includeSubsections = true);
+    public function bySectionWithSupplier($sectionId, $supplierId, $includeSubsections = true);
+
+    public function paginate();
+    public function findBy($field, $value, $columns = array('*'));
+}
