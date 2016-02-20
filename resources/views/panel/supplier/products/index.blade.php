836b4bbce1c280f9e2951653f501547d0cbcf84c
Ley Al-Khalaki <7236085@gmail.com>
2016-02-20 17:51:09 +0400

    product datatables localization

--- resources/views/panel/supplier/products/index.blade.php
+++ resources/views/panel/supplier/products/index.blade.php
@@ -12,7 +12,30 @@
                     { data: 'name', name: 'name' },
                     { data: 'created_at', name: 'created_at' },
                     { data: 'updated_at', name: 'updated_at' }
-                ]
+                ],
+                language: {
+                    "processing": "Подождите...",
+                    "search": "Поиск:",
+                    "lengthMenu": "Показать _MENU_ товаров",
+                    "info": "Товары с _START_ до _END_ из _TOTAL_ товаров",
+                    "infoEmpty": "Товары с 0 до 0 из 0 товаров",
+                    "infoFiltered": "(отфильтровано из _MAX_ товаров)",
+                    "infoPostFix": "",
+                    "loadingRecords": "Загрузка товаров...",
+                    "zeroRecords": "Товары отсутствуют.",
+                    "emptyTable": "В таблице отсутствуют данные",
+                    "paginate": {
+                        "first": "<<",
+                        "previous": "<",
+                        "next": ">",
+                        "last": ">>"
+                    },
+                    "aria": {
+                        "sortAscending": ": активировать для сортировки столбца по возрастанию",
+                        "sortDescending": ": активировать для сортировки столбца по убыванию"
+                    }
+                }
+
             });
         });
     </script>
