8fe759f05329e380706362be5ba3b76ba9df49a6
Ley Al-Khalaki <7236085@gmail.com>
2016-04-25 06:43:11 +0400

    custom 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,16 @@
+@extends('main')
+
+@section('content')
+    <div class="breadcrumb-box">
+        <a href="/">Главная</a>
+        <a href="#">Ошибка</a>
+    </div>
+
+    <div class="information-blocks">
+        <div class="container-404">
+            <div class="title">404</div>
+            <div class="description">Страница не найдена</div>
+            <div class="text">Пожалуйста, <a href="/">перейдите по ссылке</a>, чтобы продолжить покупки.</div>
+        </div>
+    </div>
+@endsection
