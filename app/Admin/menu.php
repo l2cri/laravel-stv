e8a9f90333111d5c46af60ee729e2815811c6d94
Ley Al-Khalaki <7236085@gmail.com>
2016-04-28 13:51:45 +0400

    admin menu

--- app/Admin/menu.php
+++ app/Admin/menu.php
@@ -1,8 +1,7 @@
 <?php
 
-Admin::menu()->url('/')->label('Start page')->icon('fa-dashboard');
-Admin::menu()->url('users')->label("User's list")->icon('fa-user');
-Admin::menu()->url('examples')->label("Examples db list")->icon('fa-th-list');
+Admin::menu()->url('/')->label('Главная')->icon('fa-dashboard');
+Admin::menu()->url('users')->label("Пользователи")->icon('fa-user');
 
 if (AuthUser::can('infopage_admin')) {
     Admin::menu('App\Models\Infopage')->icon('fa-info-circle')->label('Информация');
