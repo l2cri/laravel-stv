3cd3f82ca37116434e7934b2b9625dc71c6b3f9a
Ley Al-Khalaki <7236085@gmail.com>
2016-05-04 13:11:25 +0400

    banner left styles

--- resources/views/home/right-banners.blade.php
+++ resources/views/home/right-banners.blade.php
@@ -1,20 +1,13 @@
 @foreach($items as $item)
 <div class="col-lg-12 col-md-4">
     <div class="swiper-slide">
-        <div class="navigation-banner-wrapper align-1" style="background-image: url({{ url($item->image) }}); background-color: #f6e8d8;">
-            <div class="" style="height: 145px">
-                <div class="cell-view">
-                    {{--<h2 class="subtitle">Распродажа 4</h2>--}}
-                    {{--<h1 class="title">распродажа 4</h1>--}}
-                    {{--<div class="description">Описание распродажа 4.</div>--}}
-                    <div class="info">
-                        <br><br><br><br><br><br><br>
+        <div class="navigation-banner-wrapper align-1" style="background-image: url({{ url($item->image) }}); background-color: #f6e8d8; margin-bottom: 10px">
 
-                        <a class="button style-2 pull-right" href="{{$item->url}}">Подбробнее</a>
-                    </div>
+            <a href="{{$item->url}}">
+                <div class="" style="height: 145px; width: 270px">
+                    &nbsp;
                 </div>
-            </div>
-            {{--<div class="clear"></div>--}}
+            </a>
         </div>
     </div>
 </div>
